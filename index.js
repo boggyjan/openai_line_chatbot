9aa125de2973979fd13f11455c056f1768d236bc
Boggy Jang <boggy@idrip.coffee>
2022-12-07 10:02:13 +0800

    add line bot

---
+++ index.js
@@ -0,0 +1,67 @@
+import { Configuration, OpenAIApi } from 'openai'
+import * as line from '@line/bot-sdk'
+import express from 'express'
+
+function init () {
+  // line config
+  const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+  }
+
+  const app = express()
+  app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(lineEventHandler))
+      .then((result) => res.json(result))
+  })
+
+  app.get('/bot_test', async (req, res) => {
+    const result = await askOpenAI('你好，請問您的大名是？')
+    res.send(result)
+  })
+
+  app.listen(3388)
+}
+
+async function lineEventHandler (event) {
+  const lineClient = new line.Client(config)
+
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null)
+  }
+
+  const result = await askOpenAI(event.message.text)
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: result
+  })
+}
+
+async function askOpenAI (question) {
+  // opan ai config
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+  const openai = new OpenAIApi(configuration)
+
+  // request
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      max_tokens: 128,
+      prompt: question,
+      temperature: 0.9,
+    })
+
+    // res.status(200).json({ result: completion.data.choices[0].text })
+    return completion.data.choices[0].text
+  } catch (err) {
+    // console.log(err)
+    // res.status(200).json({ result: '抱歉我不知道' })
+    return null
+  }
+}
+
+init()
